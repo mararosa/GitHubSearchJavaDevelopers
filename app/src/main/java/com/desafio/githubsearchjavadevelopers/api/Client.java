4cc0099f92caaf3e3f6338375804ed10e846d605
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-13 01:37:03 -0300

    add client url

--- app/src/main/java/com/desafio/githubsearchjavadevelopers/api/Client.java
+++ app/src/main/java/com/desafio/githubsearchjavadevelopers/api/Client.java
@@ -1,4 +1,22 @@
 package com.desafio.githubsearchjavadevelopers.api;
 
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Client {
+
+    public static final String BASE_URL = "https://api.github.com";
+    public static Retrofit retrofit = null;
+
+    public static Retrofit getClients() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
