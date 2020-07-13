d9bf47ad6affb873f94e1f161b1ef3c4a28115a2
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-13 02:13:26 -0300

    add getter and setter to the item

--- app/src/main/java/com/desafio/githubsearchjavadevelopers/model/Item.java
+++ app/src/main/java/com/desafio/githubsearchjavadevelopers/model/Item.java
@@ -1,4 +1,49 @@
 package com.desafio.githubsearchjavadevelopers.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Item {
+
+    @SerializedName("login")
+    @Expose
+    private String login;
+    @SerializedName("avatar")
+    @Expose
+    private String avatarUrl;
+    @SerializedName("html_url")
+    @Expose
+    private String htmlUrl;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    public Item(String login, String avatarUrl, String htmlUrl) {
+        this.login = login;
+        this.avatarUrl = avatarUrl;
+        this.htmlUrl = htmlUrl;
+
+
+    }
 }
