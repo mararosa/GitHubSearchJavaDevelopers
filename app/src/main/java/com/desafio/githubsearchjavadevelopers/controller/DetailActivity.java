9e0e96c403da9ab361750162cff8de29aa2c400d
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-13 03:55:55 -0300

    get extras

--- app/src/main/java/com/desafio/githubsearchjavadevelopers/controller/DetailActivity.java
+++ app/src/main/java/com/desafio/githubsearchjavadevelopers/controller/DetailActivity.java
@@ -1,4 +1,40 @@
 package com.desafio.githubsearchjavadevelopers.controller;
 
-public class DetailActivity {
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.desafio.githubsearchjavadevelopers.R;
+
+import java.util.Objects;
+
+public class DetailActivity extends AppCompatActivity {
+    TextView userLink, username;
+    ImageView imageView;
+    Toolbar mActionBarToolBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        }
+
+        imageView = findViewById(R.id.detail_user_image_header);
+        username = findViewById(R.id.detail_username_tv);
+        userLink = findViewById(R.id.detail_link_tv);
+
+        String username = getIntent().getExtras().getString("login");
+        String avatarUrl = getIntent().getExtras().getString("avatar_url");
+        String link = getIntent().getExtras().getString("html_url");
+
+
+    }
 }
