4f276f9efec596b7cfc823348e4a013037871a2c
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-13 02:54:36 -0300

    add callback to the service

--- app/src/main/java/com/desafio/githubsearchjavadevelopers/controller/MainActivity.java
+++ app/src/main/java/com/desafio/githubsearchjavadevelopers/controller/MainActivity.java
@@ -1,15 +1,29 @@
 package com.desafio.githubsearchjavadevelopers.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.desafio.githubsearchjavadevelopers.ItemAdapter;
 import com.desafio.githubsearchjavadevelopers.R;
+import com.desafio.githubsearchjavadevelopers.api.Client;
+import com.desafio.githubsearchjavadevelopers.api.Service;
 import com.desafio.githubsearchjavadevelopers.model.Item;
+import com.desafio.githubsearchjavadevelopers.model.ItemResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +31,66 @@ public class MainActivity extends AppCompatActivity {
     TextView disconnected;
     private Item item;
     ProgressDialog pd;
-    private LinearLayout linearContainer;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+
+        swipeContainer = findViewById(R.id.swipeContainer);
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_orange_dark);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadJSON();
+                Toast.makeText(MainActivity.this, "GitHub Users Refreshed", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
+
+    private void loadJSON() {
+        disconnected = findViewById(R.id.main_disconnected);
+        try {
+            Client client = new Client();
+            Service apiService = Client.getClients().create((Service.class));
+            Call<ItemResponse> call = apiService.getItems();
+            call.enqueue(new Callback<ItemResponse>() {
+                @Override
+                public void onResponse(Call<ItemResponse> call, Response<ItemResponse> response) {
+                    List<Item> items = response.body().getItems();
+                    recyclerView.setAdapter(new ItemAdapter(getApplicationContext(), items));
+                    recyclerView.smoothScrollToPosition(0);
+                    swipeContainer.setRefreshing(false);
+                    pd.hide();
+
+                }
+
+                @Override
+                public void onFailure(Call<ItemResponse> call, Throwable t) {
+                    Log.d("Error", t.getMessage());
+                    Toast.makeText(MainActivity.this, "Error Fetching Data!", Toast.LENGTH_SHORT).show();
+                    disconnected.setVisibility(View.VISIBLE);
+                    pd.hide();
+                }
+            });
+        }
+    }
+
+    private void initViews() {
+        pd = new ProgressDialog(this);
+        pd.setMessage("Fetching Github users...");
+        pd.setCancelable(false);
+        pd.show();
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.smoothScrollToPosition(0);
+        loadJSON();
+    }
+
+
 }
