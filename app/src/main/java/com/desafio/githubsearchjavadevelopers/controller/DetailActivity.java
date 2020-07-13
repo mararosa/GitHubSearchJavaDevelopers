eec223ff9e1580ba241f9855c422798bc316424c
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-13 04:11:48 -0300

    inflate menu

--- app/src/main/java/com/desafio/githubsearchjavadevelopers/controller/DetailActivity.java
+++ app/src/main/java/com/desafio/githubsearchjavadevelopers/controller/DetailActivity.java
@@ -1,14 +1,21 @@
 package com.desafio.githubsearchjavadevelopers.controller;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
+import com.bumptech.glide.Glide;
 import com.desafio.githubsearchjavadevelopers.R;
 
 import java.util.Objects;
@@ -31,10 +38,37 @@ public class DetailActivity extends AppCompatActivity {
         username = findViewById(R.id.detail_username_tv);
         userLink = findViewById(R.id.detail_link_tv);
 
-        String username = getIntent().getExtras().getString("login");
+        String userLogin = getIntent().getExtras().getString("login");
         String avatarUrl = getIntent().getExtras().getString("avatar_url");
         String link = getIntent().getExtras().getString("html_url");
 
+        userLink.setText(link);
+        Linkify.addLinks(userLink, Linkify.WEB_URLS);
 
+        username.setText(userLogin);
+        Glide.with(this)
+                .load(avatarUrl)
+                .into(imageView);
+
+        getSupportActionBar().setTitle("Details Activity");
+    }
+
+    private Intent createShareForcastIntent() {
+        String userLogin = getIntent().getExtras().getString("login");
+        String link = getIntent().getExtras().getString("link");
+        Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText("Check out this awasome developer @" + userLogin + ", " + link)
+                .getIntent();
+        return shareIntent;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_action_share);
+        menuItem.setIntent(createShareForcastIntent());
+        return true;
     }
 }
