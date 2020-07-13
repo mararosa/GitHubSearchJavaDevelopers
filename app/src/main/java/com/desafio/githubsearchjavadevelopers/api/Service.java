558f20eb5db61cca80ceae4b903b44f29a4f8440
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-13 01:46:01 -0300

    add service endpoint

--- app/src/main/java/com/desafio/githubsearchjavadevelopers/api/Service.java
+++ app/src/main/java/com/desafio/githubsearchjavadevelopers/api/Service.java
@@ -1,4 +1,11 @@
 package com.desafio.githubsearchjavadevelopers.api;
 
-public class Service {
+import com.desafio.githubsearchjavadevelopers.model.ItemResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Service {
+    @GET("/search/repositories?q=language:Java&sort=forks&order=desc")
+    Call<ItemResponse> getItems();
 }
